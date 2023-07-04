221577c62450423c14296a0346acb07b7507b21c
Shivangisharma1607 <sshivangi940@gmail.com>
2023-07-04 12:53:23 +0530

    python.py

---
+++ calculator.py/calculator.py
@@ -0,0 +1,7 @@
+# calculator
+
+num1 = input("eneter the 1st number:")
+num2 = input("eneter the 2nd number:")
+result = int(num1) /int(num2)
+print(result)
+ 
---
+++ python.py/Untitled-7.py
@@ -0,0 +1,14 @@
+# create a calculator in one line of code
+print("1. Addition")
+print("2. Substraction")
+print("3. Multiplication")
+print("4. Division")
+    
+def add(x,y):
+    return x+y
+def subtract(x,y):
+    return x-y
+def multiply(x,y):
+    return x*y
+def divide(x,y):
+    return x/y
